db8f7544214241d038298c6c4b5033e97b5267f7
LeeMichael24 <00140221@uca.edu.sv>
2022-05-07 06:16:25 -0800

    CP3

---
+++ CortoPractico3/frmPrincipal.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace CortoPractico3
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSombra = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLogo2 = new System.Windows.Forms.PictureBox();
+            this.lbl3mesesGratis = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblDescripcopn = new System.Windows.Forms.Label();
+            this.btmBoton3meses = new System.Windows.Forms.Button();
+            this.nudTotal = new System.Windows.Forms.NumericUpDown();
+            this.lblTotalLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblMostrarTodo = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btmComprarPaquete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSombra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTotal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.Image")));
+            this.pictureBoxLogo.Location = new System.Drawing.Point(0, 1);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(134, 59);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 0;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // pictureBoxSombra
+            // 
+            this.pictureBoxSombra.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBoxSombra.Location = new System.Drawing.Point(133, 1);
+            this.pictureBoxSombra.Name = "pictureBoxSombra";
+            this.pictureBoxSombra.Size = new System.Drawing.Size(616, 59);
+            this.pictureBoxSombra.TabIndex = 1;
+            this.pictureBoxSombra.TabStop = false;
+            // 
+            // pictureBoxLogo2
+            // 
+            this.pictureBoxLogo2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBoxLogo2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo2.Image")));
+            this.pictureBoxLogo2.Location = new System.Drawing.Point(747, 1);
+            this.pictureBoxLogo2.Name = "pictureBoxLogo2";
+            this.pictureBoxLogo2.Size = new System.Drawing.Size(64, 59);
+            this.pictureBoxLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo2.TabIndex = 2;
+            this.pictureBoxLogo2.TabStop = false;
+            // 
+            // lbl3mesesGratis
+            // 
+            this.lbl3mesesGratis.BackColor = System.Drawing.Color.Transparent;
+            this.lbl3mesesGratis.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3mesesGratis.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl3mesesGratis.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl3mesesGratis.Location = new System.Drawing.Point(0, 103);
+            this.lbl3mesesGratis.Name = "lbl3mesesGratis";
+            this.lbl3mesesGratis.Size = new System.Drawing.Size(811, 59);
+            this.lbl3mesesGratis.TabIndex = 3;
+            this.lbl3mesesGratis.Text = "  Obtén 3 meses de Premium gratis            ";
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl1.Location = new System.Drawing.Point(0, 63);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(811, 40);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "SPOTIFY PREMIUM ";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDescripcopn
+            // 
+            this.lblDescripcopn.BackColor = System.Drawing.Color.Transparent;
+            this.lblDescripcopn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcopn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDescripcopn.Location = new System.Drawing.Point(0, 162);
+            this.lblDescripcopn.Name = "lblDescripcopn";
+            this.lblDescripcopn.Size = new System.Drawing.Size(811, 48);
+            this.lblDescripcopn.TabIndex = 5;
+            this.lblDescripcopn.Text = "        Disfruta de música sin anuncios, reproducciones en modo offline y mucho m" + "ás. Cancela eras.";
+            this.lblDescripcopn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btmBoton3meses
+            // 
+            this.btmBoton3meses.BackColor = System.Drawing.Color.Black;
+            this.btmBoton3meses.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btmBoton3meses.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btmBoton3meses.Location = new System.Drawing.Point(40, 238);
+            this.btmBoton3meses.Name = "btmBoton3meses";
+            this.btmBoton3meses.Size = new System.Drawing.Size(737, 46);
+            this.btmBoton3meses.TabIndex = 6;
+            this.btmBoton3meses.Text = "DISFRUTA DE 3 MESES GRATIS";
+            this.btmBoton3meses.UseVisualStyleBackColor = false;
+            // 
+            // nudTotal
+            // 
+            this.nudTotal.Location = new System.Drawing.Point(376, 375);
+            this.nudTotal.Maximum = new decimal(new int[] { 12, 0, 0, 0 });
+            this.nudTotal.Name = "nudTotal";
+            this.nudTotal.Size = new System.Drawing.Size(129, 22);
+            this.nudTotal.TabIndex = 7;
+            this.nudTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudTotal.ValueChanged += new System.EventHandler(this.nudSubtotal_ValueChanged);
+            // 
+            // lblTotalLabel
+            // 
+            this.lblTotalLabel.Location = new System.Drawing.Point(376, 403);
+            this.lblTotalLabel.Name = "lblTotalLabel";
+            this.lblTotalLabel.Size = new System.Drawing.Size(129, 25);
+            this.lblTotalLabel.TabIndex = 8;
+            this.lblTotalLabel.Text = "$0.00";
+            this.lblTotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(241, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Meses a contratar:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(0, 481);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(811, 48);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "        Nota: A partir de la subcripcion que elijas ya inlcuye los 3 meses gratis" + "";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Location = new System.Drawing.Point(241, 405);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(129, 20);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "Total:";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Location = new System.Drawing.Point(241, 328);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(129, 20);
+            this.lblNombre.TabIndex = 12;
+            this.lblNombre.Text = "Nombre:";
+            this.lblNombre.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblMostrarTodo
+            // 
+            this.lblMostrarTodo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblMostrarTodo.Location = new System.Drawing.Point(522, 319);
+            this.lblMostrarTodo.Name = "lblMostrarTodo";
+            this.lblMostrarTodo.Size = new System.Drawing.Size(181, 39);
+            this.lblMostrarTodo.TabIndex = 13;
+            this.lblMostrarTodo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(376, 328);
+            this.txtNombre.MaxLength = 30;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(129, 22);
+            this.txtNombre.TabIndex = 14;
+            // 
+            // btmComprarPaquete
+            // 
+            this.btmComprarPaquete.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.btmComprarPaquete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btmComprarPaquete.ForeColor = System.Drawing.Color.Black;
+            this.btmComprarPaquete.Location = new System.Drawing.Point(329, 432);
+            this.btmComprarPaquete.Name = "btmComprarPaquete";
+            this.btmComprarPaquete.Size = new System.Drawing.Size(211, 46);
+            this.btmComprarPaquete.TabIndex = 16;
+            this.btmComprarPaquete.Text = "Contratar paquete";
+            this.btmComprarPaquete.UseVisualStyleBackColor = false;
+            this.btmComprarPaquete.Click += new System.EventHandler(this.btmComprarPaquete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 578);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(811, 222);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 597);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(169, 173);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(296, 613);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(175, 149);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(571, 597);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(178, 173);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(810, 798);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btmComprarPaquete);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblMostrarTodo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotalLabel);
+            this.Controls.Add(this.nudTotal);
+            this.Controls.Add(this.btmBoton3meses);
+            this.Controls.Add(this.lblDescripcopn);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lbl3mesesGratis);
+            this.Controls.Add(this.pictureBoxLogo2);
+            this.Controls.Add(this.pictureBoxSombra);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Spotify";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSombra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTotal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btmComprarPaquete;
+
+        private System.Windows.Forms.Label lblMostrarTodo;
+
+        private System.Windows.Forms.Label lblNombre;
+
+        private System.Windows.Forms.Label lblTotal;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.NumericUpDown nudTotal;
+        private System.Windows.Forms.Label lblTotalLabel;
+
+        private System.Windows.Forms.Button btmBoton3meses;
+
+        private System.Windows.Forms.Label lblDescripcopn;
+
+        private System.Windows.Forms.Label lbl1;
+
+        private System.Windows.Forms.Label lbl3mesesGratis;
+
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.PictureBox pictureBoxSombra;
+
+        private System.Windows.Forms.PictureBox pictureBoxLogo2;
+
+        #endregion
+    }
+}
---
+++ CortoPractico3/frmPrincipal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CortoPractico3
+{
+    public partial class frmPrincipal : Form
+    {
+        private double precio = 5.99;
+        private double total = 0;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(155, 51, 153);
+        }
+
+        private void nudSubtotal_ValueChanged(object sender, EventArgs e)
+        {
+            total = precio * Convert.ToInt32(nudTotal.Value);
+            lblTotalLabel.Text = "$" + Math.Round(total, 2);
+        }
+
+
+        private void btmComprarPaquete_Click(object sender, EventArgs e)
+        {
+            lblMostrarTodo.Text = "Hola!" + txtNombre.Text + "!";
+            MessageBox.Show(text: "Felicidades! acabas de comprar un paquete de sporify sin limites",
+                caption: "Compra Exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
